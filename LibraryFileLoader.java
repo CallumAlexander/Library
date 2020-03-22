630b76aad5cb932838ab2391c027ac374715662e
Cal Alexander <s1931801@ed.ac.uk>
2020-03-22 20:13:43 +0000

    feat: add RECORD_SIZE final instance field fix: half finish parseFileContent

--- LibraryFileLoader.java
+++ LibraryFileLoader.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.StringTokenizer;
 
 /** 
  * Class responsible for loading
@@ -21,6 +23,9 @@ public class LibraryFileLoader {
      * end of each line.
      */
     private List<String> fileContent;
+
+    //The length of the data in BookEntry
+    private final int RECORD_SIZE = 5;
 
     /** Create a new loader. No file content has been loaded yet. */
     public LibraryFileLoader() { 
@@ -67,8 +72,28 @@ public class LibraryFileLoader {
      * if no book data has been loaded yet.
      * @throws UnsupportedOperationException Not implemented yet!
      */
-    public List<BookEntry> parseFileContent() {
+    public List<BookEntry> parseFileContent(List<String> fileContent) {
         // TODO Remove exception and implement me
-        throw new UnsupportedOperationException("Parsing library files is not yet implemented.");
+
+        ArrayList<BookEntry> books = new ArrayList<>();
+        String handle = "";
+        String[] dataArray = new String[RECORD_SIZE];
+        String[] authorArray = new String[];
+
+        if (fileContent == null){
+            System.err.print("ERROR: No content loaded before parsing");
+            return books;
+        }
+        for (int i = 1; i < fileContent.size(); i++){   //from index 1 to avoid the file header
+            handle = fileContent.get(i);
+            dataArray = handle.split(",");
+
+
+
+
+        }
+
+
+        // throw new UnsupportedOperationException("Parsing library files is not yet implemented.");
     }
 }
