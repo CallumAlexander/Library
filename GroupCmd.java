27d94c4e73b53d64c197c4a1f6b261c37e5699b2
Cal Alexander <s1931801@ed.ac.uk>
2020-04-01 20:43:58 +0100

    add: create a skeleton class for GroupCmd.java that extends LibraryCommand.java
    Signed-off-by: Cal Alexander <s1931801@ed.ac.uk>

--- GroupCmd.java
+++ GroupCmd.java
@@ -1,2 +1,23 @@
-public class GroupCmd {
+public class GroupCmd extends LibraryCommand {
+
+    // ** DATA **
+
+    // ** METHODS **
+
+    /**
+     * Create the specified command and initialise it with
+     * the given command argument.
+     *
+     * @param type          specific command type
+     * @param argumentInput argument input as expected by the extending subclass.
+     * @throws IllegalArgumentException if given arguments are invalid
+     * @throws NullPointerException     if any of the given parameters are null.
+     */
+    public GroupCmd(String argumentInput) { super(CommandType.GROUP, argumentInput); }
+
+
+    @Override
+    public void execute(LibraryData data) {
+
+    }
 }
